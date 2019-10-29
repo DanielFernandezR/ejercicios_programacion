d6b10554162a586dd2ec26f475fb78320372cc34
Dual <Dual@TITAN07.sinf2003srvr2.local>
2019-10-29 21:12:40 +0100

    loose_change

--- ejercicios_codewars/loose_change/loose_change.py
+++ ejercicios_codewars/loose_change/loose_change.py
@@ -1,13 +1,18 @@
-def loose_change(cents):
-    nombreMonedas = ["Pennies", "Nickels", "Dimes", "Quarters"]
-    costeMonedas = [1, 5, 10, 25]
-    wallet = dict(zip(nombreMonedas, costeMonedas))
-    assert wallet == {"Pennies": 1, "Nickels": 5, "Dimes": 10, "Quarters": 25}
+def loose_change(cambio):
+    nombre_monedas = ["Quarters", "Dimes", "Nickels", "Pennies"]
+    valores_monedas = [25, 10, 5, 1]
+    cartera = dict(zip(nombre_monedas, valores_monedas))
+    numero_monedas = []
 
-    change_dict = dict(zip(coins, exchange))
-    for numero in wallet
-    coins = []
-    exchange = []
-
-    if numero
-    return change_dict
+    if cambio <= 0:
+        return {"Nickels": 0, "Pennies": 0, "Dimes": 0, "Quarters": 0}
+    else:
+        for moneda in reversed(sorted(list(cartera.values()))):
+            uso_monedas = 0
+            while cambio >= moneda:
+                cambio -= moneda
+                uso_monedas += 1
+            numero_monedas.append(uso_monedas)
+            
+    cartera = dict(zip(nombre_monedas, numero_monedas))
+    return cartera
---
+++ ejercicios_codewars/loose_change/test_loose_change.py
@@ -0,0 +1,12 @@
+from loose_change import loose_change
+
+def test_caso_test():
+    assert loose_change(29) == {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 1}
+def test_caso_test2():
+    assert loose_change(91) == {'Nickels': 1, 'Pennies': 1, 'Dimes': 1, 'Quarters': 3}
+def test_caso_test3():
+    assert loose_change(0) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+def test_caso_test4():
+    assert loose_change(-2) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+def test_caso_test5():
+    assert loose_change(3.9) == {'Nickels': 0, 'Pennies': 3, 'Dimes': 0, 'Quarters': 0}
